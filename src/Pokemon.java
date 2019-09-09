20248b64f94145bd51d3be1aa778f75996beec9b
do_tha000 <do_tha000@CAM-LAB-12.bgsd.k12.wa.us>
2019-09-09 07:26:30 -0700

    Finished Pokemon

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -3,7 +3,7 @@ public class Pokemon {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Pikachu welcomes you to the world of Pokemon!\n (\\__/)\n (o^.^)\nz(_(\")(\")");
 	}
 
 }
